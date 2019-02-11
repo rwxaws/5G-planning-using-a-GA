043a8cb9a450f6bfe95e0f8813d17ea877b21a7a
Aws A <pisceswolf96@gmail.com>
2019-02-11 23:42:26 +0300

    implement methods to calculate max and min users in each cell

--- files/objs/cell.py
+++ files/objs/cell.py
@@ -10,7 +10,9 @@ class Cell:
         self.__cell_type = cell_type
         self.__connected_users = []
         self.__state = True
-        self.__cost = self.calculate_cost()
+        self.__cost = self.__calculate_cost()
+        self.__min_users = self.__calculate_min_users()
+        self.__max_users = self.__calculate_max_users()
 
     # getters
     def get_xcoord(self):
@@ -19,8 +21,8 @@ class Cell:
     def get_ycoord(self):
         return self.__ycoord
 
-    def get_connected_users(self):
-        return self.__connected_users
+    def get_num_connected_users(self):
+        return len(self.__connected_users)
 
     def get_cell_type(self):
         return self.__cell_type
@@ -45,13 +47,32 @@ class Cell:
     def calculate_radius(self):
         pass
 
-    def calculate_cost(self):
+    def __calculate_cost(self):
         if self.__cell_type == "macro":
             return 175
         elif self.__cell_type == "micro":
             return 25
         else:
             return 5
+
+    def __calculate_min_users(self):
+        type = self.get_cell_type()
+        if type == "macro":
+            return 0
+        elif type == "micro":
+            return 5
+
+    def __calculate_max_users(self):
+        type = self.get_cell_type()
+        if type == "macro":
+            return 20
+        elif type == "micro":
+            return 15
+
+    def is_available(self):
+        if self.get_num_connected_users() < self.__max_users:
+            return True
+        return False
 
     def pprint(self):
         return """
