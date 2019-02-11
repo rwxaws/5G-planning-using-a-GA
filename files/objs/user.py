4f4f217dccc57da525113173ce4a7f5a211ee52c
Aws A <pisceswolf96@gmail.com>
2019-02-11 23:12:02 +0300

    add user basestation properties

--- files/objs/user.py
+++ files/objs/user.py
@@ -3,7 +3,8 @@ class User:
     def __init__(self, coord_x, coord_y):
         self.__coord_x = coord_x
         self.__coord_y = coord_y
-        self.__received_power_vector = None
+        self.__close_bss = None
+        self.__connected_bs = None
 
     # getters
     def get_xcoord(self):
@@ -12,5 +13,8 @@ class User:
     def get_ycoord(self):
         return self.__coord_y
 
-    def get_recieved_power_vector(self):
-        return self.__received_power_vector
+    def get_close_bss(self):
+        return self.__close_bss
+
+    def get_connected_bs(self):
+        return self.__connected_bs
