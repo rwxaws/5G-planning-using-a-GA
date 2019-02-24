eeff4d69e70ad4df3e74a5c5ba2c8052773cf71c
Aws A <pisceswolf96@gmail.com>
2019-02-24 09:05:20 +0300

    add connect_users function to helper.

--- files/helper_funcs/helper.py
+++ files/helper_funcs/helper.py
@@ -3,7 +3,7 @@ from copy import copy
 from ..objs.cell import Cell
 from ..objs.user import User
 from ..objs.plan import Plan
-from ..network.net_funcs import distance
+from ..network.net_funcs import distance, received_power
 
 
 def generate_cells(candidate_points_list, type_of_cell, distance_between_cells, num_of_cells):
@@ -129,6 +129,51 @@ def generate_initial_population(num_of_plans,
         micro_cells = []
         cp = []
     return pool
+
+
+def connect_users(pool, num_macro, num_micro):
+    """Connect users of each plan in pool to available cellular cells."""
+
+    # iterate over every plan
+    for plan in pool:
+        for user in plan.get_users():
+            user.empty_close_bss()
+            for cell in plan.get_cells():
+                # if user is within the radius of the cell
+                if distance(user.get_xcoord(), user.get_ycoord(), cell.get_xcoord(),
+                            cell.get_ycoord()) < cell.get_radius():
+                    if cell.is_available():
+                        user.add_to_close_bss(cell)
+
+            # if user is within at least one base station range
+            if len(user.get_close_bss()) > 0:
+                desired = user.get_close_bss()[0]
+                for tested_cell in user.get_close_bss()[1:]:
+                    dist1 = distance(desired.get_xcoord(),
+                                     desired.get_ycoord(),
+                                     user.get_xcoord(),
+                                     user.get_ycoord())
+                    dist2 = distance(tested_cell.get_xcoord(),
+                                     tested_cell.get_ycoord(),
+                                     user.get_xcoord(),
+                                     user.get_ycoord())
+
+                    num_bs = num_macro if desired.get_cell_type() == "macro" else num_micro
+                    power1 = received_power(desired.get_power(),
+                                            num_bs,
+                                            dist1,
+                                            desired.get_frequency(), 0, 0)
+
+                    num_bs = num_macro if tested_cell.get_cell_type() == "macro" else num_micro
+                    power2 = received_power(tested_cell.get_power(),
+                                            num_bs,
+                                            dist2,
+                                            tested_cell.get_frequency(), 0, 0)
+
+                    if power2 > power1:
+                        desired = tested_cell
+                user.set_connected_bs(desired)
+                desired.add_user(user)
 
 
 def print_pop(population_pool, num_macro, num_micro):
