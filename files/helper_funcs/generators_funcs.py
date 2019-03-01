eea8ca5f05e74d1656fbce2d6ae402d50a41593f
Aws A <pisceswolf96@gmail.com>
2019-03-01 18:50:56 +0300

    move generator functions into a new file

---
+++ files/helper_funcs/generators_funcs.py
@@ -0,0 +1,134 @@
+import copy
+import numpy as np
+
+from ..objs.user import User
+from ..objs.plan import Plan
+from ..objs.cell import Cell
+from ..network.net_funcs import distance
+from .helper import within
+
+
+def generate_cells(candidate_points_list, type_of_cell, distance_between_cells, num_of_cells):
+    """Generates cells using the given candidate points.
+
+    Generates required cells that can be used to create a population.
+
+    Args:
+        candidate_points_list: the list of candidate points each as a tuple
+        type_of_cell: a string representing the type of cell(eg: macro)
+                    that is to be generated.
+        distance_between_cells: the distance (in meters) between every cell.
+        num_of_cells: how many cells to be generated.
+    """
+    np.random.shuffle(candidate_points_list)
+    temp_candidate_points_list = copy.deepcopy(candidate_points_list)
+    cell_list = []
+
+    # append the first cell (needed to ensure that we are not looping over an empty list)
+    cell_coords = candidate_points_list.pop()
+    cell = Cell(cell_coords[0], cell_coords[1], type_of_cell)
+    cell_list.append(cell)
+
+    # look for the remainding candidate points
+    for cp in temp_candidate_points_list:
+        is_well_positioned = True
+        for c in cell_list:
+            if distance(cp[0], c.get_xcoord(), cp[1], c.get_ycoord()) < distance_between_cells:
+                is_well_positioned = False
+                break
+
+        if is_well_positioned:
+            cell = Cell(cp[0], cp[1], type_of_cell)
+            cell_list.append(cell)
+            candidate_points_list.pop(candidate_points_list.index(cp))
+
+        if len(cell_list) >= num_of_cells:
+            break
+    return cell_list
+
+
+def generate_users(num_of_users, area):
+    """Generate users in a uniform random way."""
+    users = []
+    for _ in range(num_of_users):
+        x = round(np.random.uniform(0, area))
+        y = round(np.random.uniform(0, area))
+        user = User(x, y)
+        users.append(user)
+    return users
+
+
+def generate_candidate_points(area, step, users_list, users_threshold):
+    """Generate candidate points in a uniform random way."""
+    candidate_points = []
+    for i in range(0, area, step):
+        for j in range(0, area, step):
+            users_num = 0
+            for user in users_list:
+                if within(i, j, step, user.get_xcoord(), user.get_ycoord()):
+                    users_num += 1
+
+            if users_num >= users_threshold:
+                candidate_point_x = round(np.random.uniform(i, i + step))
+                candidate_point_y = round(np.random.uniform(j, j + step))
+                candidate_points.append((candidate_point_x, candidate_point_y))
+    return candidate_points
+
+
+def generate_initial_population(num_of_plans,
+                                candidate_points,
+                                users,
+                                num_macro,
+                                distance_macro,
+                                num_micro,
+                                distance_micro,
+                                num_pico=None,
+                                distance_pico=None,
+                                num_femto=None,
+                                distance_femto=None):
+    """Generate the initial population.
+
+    Args:
+        num_of_plans: An integer of the size of the population.
+        candidate_points: A list of candidate points.
+        users: A list of users.
+        num_macro: An integer of the number of macro cells.
+        distance_macro: An integer of the distance between each macro cell.
+        num_micro: An integer of the number of micro cells.
+        distance_macro: An integer of the distance between each micro cell.
+        num_pico: An integer of the number of pico cells.
+        distance_pico: An integer of the distance between each pico cell.
+        num_femto: An integer of the number of femto cells.
+        distance_femto: An integer of the distance between each femto cell.
+
+    Returns:
+        A list(pool) of plans.
+    """
+
+    pool = []
+    for _ in range(num_of_plans):
+        cp = copy.deepcopy(candidate_points)
+        np.random.shuffle(cp)
+
+        # generate cells
+        macro_cells = generate_cells(cp,
+                                     "macro",
+                                     distance_macro,
+                                     num_macro)
+        micro_cells = generate_cells(cp,
+                                     "micro",
+                                     distance_micro,
+                                     num_micro)
+
+        # generate a new plan
+        plan = Plan(macro_cells + micro_cells,
+                    copy.deepcopy(users),
+                    copy.deepcopy(cp),
+                    num_macro,
+                    num_micro)
+        pool.append(plan)
+
+        macro_cells = []
+        micro_cells = []
+        cp = []
+    return pool
--- files/main.py
+++ files/main.py
@@ -1,25 +1,27 @@
 from .objs.cell import Cell
 from .objs.user import User
 from .objs.plan import Plan
-from .helper_funcs.helper import generate_users, generate_candidate_points, generate_initial_population
+from .helper_funcs.generators_funcs import (generate_users,
+                                            generate_candidate_points,
+                                            generate_initial_population)
 
-import matplotlib.pyplot as plt
 import numpy as np
-from copy import copy
+import copy
 
 
 # constants
-NUM_OF_USERS = 850
 AREA = 3000
 STEP = 250
 USERS_THRESHOLD = 5
+NUM_USERS = 850
+NUM_CHROMOSOMES = 5
 NUM_MACROCELLS = 5
 NUM_MICROCELLS = 10
 DISTANCE_MACROCELLS = 1000
 DISTANCE_MICROCELLS = 100
 FREQ_MACRO = 3.5
 FREQ_SMALL_CELLS = 28
-# NUM_OF_USERS = int(input("Enter the number of users:"))
+# NUM_USERS = int(input("Enter the number of users:"))
 # AREA = int(input("Enter the search area:"))
 # STEP = int(input("Enter step size:"))
 # USERS_THRESHOLD = int(input("Enter users threshold:"))
@@ -36,59 +38,17 @@ micro_cells = []
 pool = []
 
 # generate users
-users = generate_users(NUM_OF_USERS, AREA)
+users = generate_users(NUM_USERS, AREA)
 
 # generate candidate points
 candidate_points = generate_candidate_points(
-    AREA, STEP, copy(users), USERS_THRESHOLD)
+    AREA, STEP, copy.deepcopy(users), USERS_THRESHOLD)
 
 
 # generate initial population
-pool = generate_initial_population(10,
+pool = generate_initial_population(NUM_CHROMOSOMES,
                                    candidate_points, users,
                                    NUM_MACROCELLS,
                                    DISTANCE_MACROCELLS,
                                    NUM_MICROCELLS,
                                    DISTANCE_MICROCELLS)
-
-for plan in pool:
-    plan.pprint()
-
-
-users_x_positions = [user.get_xcoord() for user in users]
-users_y_positions = [user.get_ycoord() for user in users]
-
-# candidate_point_x, candidate_point_y = zip(*candidate_points)
-candidate_point_x1, candidate_point_y1 = zip(*pool[0].get_candidate_points())
-candidate_point_x2, candidate_point_y2 = zip(*pool[1].get_candidate_points())
-
-
-f1 = plt.figure(1)
-plt.grid(which="major", axis="both", linewidth=2)
-plt.xticks(np.arange(0, AREA + STEP, STEP))
-plt.yticks(np.arange(0, AREA + STEP, STEP))
-
-plt.plot(users_x_positions, users_y_positions, "k.", label="users")
-plt.plot(candidate_point_x1, candidate_point_y1, "ro", label="candidate points")
-# plt.plot(macrocells_x_positions, macrocells_y_positions,
-#          "bX", label="macro cells")
-# plt.plot(microcells_x_positions, microcells_y_positions,
-#          "g^", label="micro cells")
-plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
-f1.show()
-
-f2 = plt.figure(2)
-plt.grid(which="major", axis="both", linewidth=2)
-plt.xticks(np.arange(0, AREA + STEP, STEP))
-plt.yticks(np.arange(0, AREA + STEP, STEP))
-
-plt.plot(users_x_positions, users_y_positions, "k.", label="users")
-plt.plot(candidate_point_x2, candidate_point_y2, "ro", label="candidate points")
-# plt.plot(macrocells_x_positions, macrocells_y_positions,
-#          "bX", label="macro cells")
-# plt.plot(microcells_x_positions, microcells_y_positions,
-#          "g^", label="micro cells")
-plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
-f2.show()
-
-input()
