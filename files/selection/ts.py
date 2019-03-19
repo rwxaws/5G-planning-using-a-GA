29c099affd481a2e0a59bf8de19a59cbcaffbed9
Aws A <pisceswolf96@gmail.com>
2019-03-19 21:28:02 +0300

    implement tournament selection.

---
+++ files/selection/ts.py
@@ -0,0 +1,22 @@
+import numpy.random as nprandom
+import copy
+
+
+def tournament_selection(pool):
+    old_pool = copy.deepcopy(pool)
+    new_pool = []
+
+    for _ in range(len(pool)):
+        f1 = nprandom.choice(old_pool)
+        f2 = nprandom.choice(old_pool)
+
+        print("**********")
+        print(f1.get_fitness())
+        print(f2.get_fitness())
+        print("**********\n")
+
+        if f1.get_fitness() >= f2.get_fitness():
+            new_pool.append(f1)
+        else:
+            new_pool.append(f2)
+    return new_pool
