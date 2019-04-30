26df0b526a04a2d8eeda6c97f1358e8c6d6b2b44
Aws A <pisceswolf96@gmail.com>
2019-05-01 00:00:50 +0300

    remove print_pop from helper

--- files/helper_funcs/helper.py
+++ files/helper_funcs/helper.py
@@ -1,27 +1,12 @@
 import copy
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 from ..network.net_funcs import distance, received_power
 from ..objs.cell import Cell
 from ..objs.plan import Plan
 from ..objs.user import User
-
-
-def print_pop(population_pool, num_macro, num_micro):
-    """Print the population in a human readable format."""
-    print("##########")
-    print("MACRO CELLS")
-    for macro_cell in population_pool[0:num_macro]:
-        print(macro_cell.pprint())
-    print("##########\n")
-
-    print("##########")
-    print("MICRO CELLS")
-    for micro_cell in population_pool[num_macro: num_macro + num_micro]:
-        print(micro_cell.pprint())
-    print("##########\n")
 
 
 def within(x, y, size, px, py):
