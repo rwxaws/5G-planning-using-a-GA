bb1020470be1b0aac8eb281cad3f507fc24a3ae9
pisceswolf96 <pisceswolf96@gmail.com>
2019-12-09 20:11:13 +0300

    add main file as a starting point

--- files/main.py
+++ files/main.py
@@ -1,48 +1,21 @@
 import copy
 import csv
 import random
+import os
 
 from .helper_funcs.generators_funcs import (generate_candidate_points,
                                             generate_initial_population,
                                             generate_users)
 from .helper_funcs.helper import (find_best_plan, output_plans)
+from .selection.selection import selection
 from .crossover.crossover import crossover
 from .mutation.mutation import mutation
-from .consts.constants import (AREA,
-                               STEP_SIZE,
-                               USERS_THRESHOLD,
-                               NUM_GENERATIONS,
-                               NUM_CHROMOSOMES,
-                               NUM_USERS,
-
-                               NUM_FIXED_MACRO,
-                               NUM_MACRO,
-                               NUM_MICRO,
-                               NUM_PICO,
-                               NUM_FEMTO,
-
-                               FIXED_MACRO_RADIUS,
-                               MACRO_RADIUS,
-                               MICRO_RADIUS,
-                               PICO_RADIUS,
-                               FEMTO_RADIUS,
-
-                               FIXED_MACRO_FREQ,
-                               MACRO_FREQ,
-                               SMALL_CELL_FREQ,
-                               
-                               CROSSOVER_PROBABILTY,
-                               MUTATION_PROBABILTY,
-                               CROSSOVER_METHOD,
-                               MUTATION_METHOD,
-                               ALPHA)
+from .consts.constants import *
 
 
 # lists
 users = []
 candidate_points = []
-macro_cells = []
-micro_cells = []
 pool = []
 best_plans = []
 
@@ -72,13 +45,21 @@ pool = generate_initial_population(NUM_CHROMOSOMES,
                                    FEMTO_RADIUS)
 
 for generation in range(NUM_GENERATIONS):
-    print("GENERATION #{}".format(generation)) for plan in pool:
+    print("GENERATION #{}".format(generation)) 
+    for plan in pool:
         plan.operate()
         print(plan.pprint())
 
+    pool = selection(pool, SELECTION_METHOD)
+
     cross_point = random.randint(1, len(pool) - 1)
     pool = crossover(pool, CROSSOVER_PROBABILTY, cross_point, CROSSOVER_METHOD, ALPHA)
+
     mutation(pool, AREA, MUTATION_PROBABILTY, MUTATION_METHOD) 
+
     best_plans.append(find_best_plan(pool))
 
+
+if "figs" not in os.listdir("files"):
+    os.mkdir("files/figs")
 output_plans(best_plans)
